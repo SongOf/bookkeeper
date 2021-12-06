3e79aa60be35ee50523bac1537cb7e99c847f729
lushiji <lushiji@didiglobal.com>
2021-12-06 20:10:52 +0800

    bug fix for EntryLocationIndex.getLastEntryInLedger https://github.com/apache/bookkeeper/pull/2928

---
+++ tests/integration/cluster/src/test/java/org/apache/bookkeeper/tests/integration/starter/BKClientTest.java
@@ -0,0 +1,193 @@
+package org.apache.bookkeeper.tests.integration.starter;
+
+import org.apache.bookkeeper.client.BookKeeper;
+import org.apache.bookkeeper.client.LedgerEntry;
+import org.apache.bookkeeper.client.LedgerHandle;
+import org.apache.bookkeeper.conf.ClientConfiguration;
+import org.junit.AfterClass;
+import org.testng.annotations.Test;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+import org.apache.bookkeeper.client.AsyncCallback.OpenCallback;
+
+
+public class BKClientTest2 {
+    static ClientConfiguration cc = null;
+    static BookKeeper bkc = null;
+    static LedgerHandle lh = null;
+    static int numberOfEntries = 5;
+    static int ensSize = 3, writeQuorumSize = 2, ackQuorumSize = 2;
+    static byte[] ledgerPassword;
+    static long ledgerId = 14; //lh.getId();
+    OpenCallback opencb=null;
+
+    public void initOpenCallback(){
+        opencb = (rc, lh, ctx1) -> {
+            System.out.println("test");
+        };
+    }
+
+    @Test
+    public void clientAddTest() throws Exception {
+        try {
+            // Create a client object for the local ensemble. This
+            // operation throws multiple exceptions, so make sure to
+            // use a try/catch block when instantiating client objects.
+            String servers = "localhost:2181";
+//            String servers = "10.179.45.65:2181,10.179.45.66:2181,10.179.45.67:2181/iot-bk";
+
+            int timeout = 60 * 1000;
+            cc = new ClientConfiguration().setMetadataServiceUri("zk+null://" + servers + "/ledgers");
+            cc.setZkTimeout(timeout);
+            cc.setAddEntryQuorumTimeout(timeout);
+
+            bkc = new BookKeeper(cc);
+            // A password for the new ledger
+            ledgerPassword = "".getBytes(StandardCharsets.UTF_8); /* some sequence of bytes, perhaps random */
+            lh = bkc.createLedger(ensSize, writeQuorumSize, ackQuorumSize, BookKeeper.DigestType.MAC, ledgerPassword);
+            long ledgerId = lh.getId();
+            // Add entries to the ledger, then close it
+            for (int i = 0; i < numberOfEntries; i++) {
+                long addEntry = lh.addEntry(("Some entry data" + i).getBytes(StandardCharsets.UTF_8));
+                System.out.println("addEntry==" + addEntry);
+            }
+            lh.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void dataRecoveryLedgerTest() throws Exception {
+        BookKeeper bkc2 = null;
+        try {
+            initOpenCallback();
+            String servers = "localhost:2181";
+////            String servers = "10.179.45.65:2181,10.179.45.66:2181,10.179.45.67:2181/iot-bk";
+//
+            int timeout = 600 * 1000;
+            cc = new ClientConfiguration().setMetadataServiceUri("zk+null://" + servers + "/ledgers");
+            cc.setZkTimeout(timeout);
+            cc.setAddEntryTimeout(timeout);
+            cc.setAddEntryQuorumTimeout(timeout);
+            bkc = new BookKeeper(cc);
+//            // A password for the new ledger
+            ledgerPassword = "".getBytes(StandardCharsets.UTF_8); /* some sequence of bytes, perhaps random */
+            lh = bkc.createLedger(ensSize, writeQuorumSize, ackQuorumSize, BookKeeper.DigestType.MAC, ledgerPassword);
+            long ledgerId = lh.getId();
+            for (int i = 0; i < numberOfEntries; i++) {
+                long addEntry = lh.addEntry(("Some entry data" + i).getBytes(StandardCharsets.UTF_8));
+                System.out.println("addEntry==" + addEntry);
+            }
+
+            ClientConfiguration cc2 = new ClientConfiguration().setMetadataServiceUri("zk+null://" + servers + "/ledgers");
+            cc2.setZkTimeout(timeout);
+            cc2.setAddEntryTimeout(timeout);
+            cc2.setAddEntryQuorumTimeout(timeout);
+            cc2.setReadEntryTimeout(timeout);
+            Thread.sleep(5000);
+            bkc2 = new BookKeeper(cc2);
+//            long ledgerId =9;
+            bkc2.asyncOpenLedger(ledgerId, BookKeeper.DigestType.MAC, ledgerPassword, opencb, null);
+            Thread.sleep(100000000);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lh.close();
+            bkc.close();
+            if (bkc2!=null) {
+                bkc2.close();
+            }
+        }
+    }
+
+    @Test
+    public void noDataRecoveryLedgerTest() throws Exception {
+        BookKeeper bkc2 = null;
+        try {
+            initOpenCallback();
+            String servers = "localhost:2181";
+////            String servers = "10.179.45.65:2181,10.179.45.66:2181,10.179.45.67:2181/iot-bk";
+//
+            int timeout = 600 * 1000;
+            cc = new ClientConfiguration().setMetadataServiceUri("zk+null://" + servers + "/ledgers");
+            cc.setZkTimeout(timeout);
+            cc.setAddEntryTimeout(timeout);
+            cc.setAddEntryQuorumTimeout(timeout);
+            bkc = new BookKeeper(cc);
+//            // A password for the new ledger
+            ledgerPassword = "".getBytes(StandardCharsets.UTF_8); /* some sequence of bytes, perhaps random */
+            lh = bkc.createLedger(ensSize, writeQuorumSize, ackQuorumSize, BookKeeper.DigestType.MAC, ledgerPassword);
+            long ledgerId = lh.getId();
+//            for (int i = 0; i < numberOfEntries; i++) {
+//                long addEntry = lh.addEntry(("Some entry data" + i).getBytes(StandardCharsets.UTF_8));
+//                System.out.println("addEntry==" + addEntry);
+//            }
+
+            ClientConfiguration cc2 = new ClientConfiguration().setMetadataServiceUri("zk+null://" + servers + "/ledgers");
+            cc2.setZkTimeout(timeout);
+            cc2.setAddEntryTimeout(timeout);
+            cc2.setAddEntryQuorumTimeout(timeout);
+            cc2.setReadEntryTimeout(timeout);
+            Thread.sleep(5000);
+            bkc2 = new BookKeeper(cc2);
+//            long ledgerId =9;
+            bkc2.asyncOpenLedger(ledgerId, BookKeeper.DigestType.MAC, ledgerPassword, opencb, null);
+            Thread.sleep(100000000);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lh.close();
+            bkc.close();
+            if (bkc2!=null) {
+                bkc2.close();
+            }
+        }
+    }
+
+    @Test
+    public void clientReadTest() throws Exception {
+        try {
+            // Create a client object for the local ensemble. This
+            // operation throws multiple exceptions, so make sure to
+            // use a try/catch block when instantiating client objects.
+            String servers = "localhost:2181";
+            int timeout = 60 * 1000;
+            cc = new ClientConfiguration().setMetadataServiceUri("zk+null://" + servers + "/ledgers");
+            cc.setZkTimeout(timeout);
+            cc.setReadEntryTimeout(timeout);
+
+            bkc = new BookKeeper(cc);
+            // A password for the new ledger
+            ledgerPassword = "".getBytes(StandardCharsets.UTF_8); /* some sequence of bytes, perhaps random */
+            // Open the ledger for reading
+            lh = bkc.openLedger(ledgerId, BookKeeper.DigestType.MAC, ledgerPassword);
+            // Read all available entries
+            Enumeration<LedgerEntry> entries = lh.readEntries(0, numberOfEntries - 1);
+            while (entries.hasMoreElements()) {
+                ByteBuffer result = ByteBuffer.wrap(entries.nextElement().getEntry());
+                Integer retrEntry = result.getInt();
+
+                // Print the integer stored in each entry
+                System.out.println(String.format("Result: %s", retrEntry));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterClass
+    public void close() throws Exception {
+        // Close the ledger and the client
+        if (lh != null) {
+            lh.close();
+        }
+        if (bkc != null) {
+            bkc.close();
+        }
+    }
+}
---
+++ tests/integration/cluster/src/test/java/org/apache/bookkeeper/tests/integration/starter/BookKeeperStartTest.java
@@ -0,0 +1,88 @@
+package org.apache.bookkeeper.tests.integration.starter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.bookkeeper.server.Main;
+import org.apache.bookkeeper.shims.zk.ZooKeeperServerShim;
+import org.apache.bookkeeper.stream.cluster.StandaloneStarter;
+import org.apache.bookkeeper.util.IOUtils;
+import org.apache.bookkeeper.util.LocalBookKeeper;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by lushiji@didichuxing.com on 2021/11/04.
+ * bk 所有启动方式集合
+ */
+@Slf4j
+public class BookKeeperStartTest {
+    public final static String BK_HOME = "/Users/lushiji-dd-mac/idea_projects/pulsar_all/bookkeeper";
+
+    @Test
+    public void testLocal() {
+        /**
+         * org.apache.bookkeeper.util.LocalBookKeeper 好多依赖的dependency的scope都为test，此类方便本地调试使用
+         */
+        String[] args = new String[]{"1", BK_HOME + "/conf/bookkeeper_lc.conf"};
+        LocalBookKeeper.main(args);
+    }
+
+    @Test
+    public void testStandalone() throws Exception {
+        String[] args = new String[]{"--conf", BK_HOME + "/conf/standalone.conf"};
+        StandaloneStarter.main(args);
+    }
+
+    private ZooKeeperServerShim zks = null;
+
+    public void startZK() {
+        try {
+            String dirSuffix = "test";
+            int zkPort = 2181;
+            File zkTmpDir = null;
+            File zkDataDirFile = null;
+            zkTmpDir = IOUtils.createTempDir("zookeeper", dirSuffix, zkDataDirFile);
+            zkTmpDir.deleteOnExit();
+            zks = LocalBookKeeper.runZookeeper(1000, zkPort, zkTmpDir);
+        } catch (IOException e) {
+            log.error("startZK has error.", e);
+        }
+    }
+
+    public void stopZK() {
+        if (null != zks) {
+            zks.stop();
+        }
+    }
+
+    @Test
+    public void testMultiCluster2() throws Exception {
+        String[] args2 = new String[]{"--conf", BK_HOME + "/conf/local/bookkeeper_lc2.conf"};
+        Main.main(args2);
+        System.out.println("bk2 started...");
+    }
+
+    @Test
+    public void testMultiCluster3() throws Exception {
+
+        String[] args3 = new String[]{"--conf", BK_HOME + "/conf/local/bookkeeper_lc3.conf"};
+        Main.main(args3);
+        System.out.println("bk3 started...");
+    }
+
+    @Test
+    public void testCluster() throws Exception {
+//        startZK();
+//        String[] args = new String[]{"--conf", BK_HOME + "/conf/bookkeeper_lc.conf"};
+        String[] args = new String[]{"--conf", BK_HOME + "/conf/local/bookkeeper_lc1.conf"};
+        Main.main(args);
+//        stopZK();
+    }
+
+    @Test
+    public void testMainHelp() throws Exception {
+        String[] args = new String[]{"--help"};
+        Main.main(args);
+    }
+}
