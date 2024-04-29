0cb2c83ddd3fab21d551ad32cdb635872d30381d
wenbingshen <oliver.shen999@gmail.com>
2024-04-29 11:13:34 +0800

    Prevent bookie shutdown due to rest api when bookie prohibits readOnlyMode (#3972)

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/BookieStateManager.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/BookieStateManager.java
@@ -190,6 +190,11 @@ public class BookieStateManager implements StateManager {
     }
 
     @Override
+    public boolean isReadOnlyModeEnabled() {
+        return conf.isReadOnlyModeEnabled();
+    }
+
+    @Override
     public boolean isAvailableForHighPriorityWrites() {
         return availableForHighPriorityWrites;
     }
