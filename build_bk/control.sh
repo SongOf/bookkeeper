05e93ad11cc4806a5d43917dbd2ebec470d1e41b
congbo <congbo@didiglobal.com>
2024-12-17 19:30:43 +0800

    add control check

--- build_bk/control.sh
+++ build_bk/control.sh
@@ -650,6 +650,14 @@ function init_server_info() {
              check_ssd 'data10'
          fi
         BOOKIE_CONF="${BOOKIE_HOME}/conf/bk_conf/bookkeeper.level3-S2103-air.gz01.conf"
+     elif [[ ${SERVICE_CLUSTER_NAME} == us01-v.level2-S2103-pro.bookkeeper.dop.ddmq.didi.com ]]; then
+         if [[ ${is_check_ssd} == "true" ]];then
+             check_ssd 'data0'
+             check_ssd 'data1'
+             check_ssd 'data5'
+             check_ssd 'data10'
+         fi
+        BOOKIE_CONF="${BOOKIE_HOME}/conf/bk_conf/bookkeeper.level2-S2103-pro.us01.conf"
      fi
      export SERVICE_NAME
      export CLUSTER_NAME
