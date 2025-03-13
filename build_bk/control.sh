bb82f81240f3303332a71ede54c452f4b0f6aa9c
houbonan <houbonan@didiglobal.com>
2025-03-13 11:05:12 +0800

    add level2-s2103-pro-ys cluster

--- build_bk/control.sh
+++ build_bk/control.sh
@@ -658,7 +658,16 @@ function init_server_info() {
              check_ssd 'data10'
          fi
         BOOKIE_CONF="${BOOKIE_HOME}/conf/bk_conf/bookkeeper.level2-S2103-pro.us01.conf"
+     elif [[ ${SERVICE_CLUSTER_NAME} == hnb-v.level2-S2103-pro.bookkeeper.dop.ddmq.didi.com ]]; then
+         if [[ ${is_check_ssd} == "true" ]];then
+             check_ssd 'data0'
+             check_ssd 'data1'
+             check_ssd 'data5'
+             check_ssd 'data10'
+         fi
+        BOOKIE_CONF="${BOOKIE_HOME}/conf/bk_conf/bookkeeper.level2-S2103-pro.ys.conf"
      fi
+
      export SERVICE_NAME
      export CLUSTER_NAME
      export SERVICE_CLUSTER_NAME
